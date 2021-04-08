8f92707460b5b60d922b3a8d388fdb066119f913
Rujuu-prog <81368541+Rujuu-prog@users.noreply.github.com>
2021-04-08 15:19:13 +0900

    add jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,15 @@ const App = () => {
     color: "blue",
     fontSize: "18px"
   };
+  const contentLadyStyle = {
+    color: "pink",
+    fontSize: "18px"
+  };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！！</h1>
       <p style={contentStyle}>オゲンキデスカ</p>
+      <p style={contentLadyStyle}>元気です</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
